a9b6880cd25311c50a831e139d51fa6839daf966
Kilian Sprotte <kilian.sprotte@gmail.com>
2010-08-05 21:00:41 +0200

    unused

--- Utils.hs
+++ Utils.hs
@@ -6,11 +6,3 @@ isForAllNeighbours pred list = and (map (uncurry pred) (neighbours list))
 
 intToFloat :: Int -> Float
 intToFloat n = fromInteger (toInteger n)
-
-binsearch xs value low high
-   | high < low       = Nothing
-   | xs!!mid > value  = binsearch xs value low (mid-1)
-   | xs!!mid < value  = binsearch xs value (mid+1) high
-   | otherwise        = Just mid
-   where
-   mid = low + ((high - low) `div` 2)
