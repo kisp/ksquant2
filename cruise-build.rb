e37a80ae9f3f6138623a0b1d4efefdba831840d6
Kilian Sprotte <kilian.sprotte@gmail.com>
2010-08-05 19:26:21 +0200

    cc

--- cruise-build.rb
+++ cruise-build.rb
@@ -11,7 +11,7 @@ end
 
 safe_system "make clean"
 safe_system "make build >build.log 2>&1"
-safe_system "mv build.log #{ENV['CC_BUILD_ARTIFACTS']}/build.log"
+safe_system "mv build.log #{ENV['CC_BUILD_ARTIFACTS']}/xxbuild.log"
 
 safe_system "make doc"
 safe_system "mv 'dist/doc/html/haskell-quant/haskell-quant' #{ENV['CC_BUILD_ARTIFACTS']}/doc"
