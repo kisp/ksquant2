60475645e4f20b26a3a2fd8d37afd8f660d674ae
Kilian Sprotte <kilian.sprotte@gmail.com>
2010-08-07 10:04:42 +0200

    cruise build 'if ENV['CC_BUILD_ARTIFACTS']"

--- cruise-build.rb
+++ cruise-build.rb
@@ -15,19 +15,19 @@ def make(target)
   $counter += 1
   name = "#{$counter}-make-#{target}.log"
   safe_system "make #{target} >#{name} 2>&1 && touch #{name}"
-  safe_system "mv #{name} #{ENV['CC_BUILD_ARTIFACTS']}/#{name}"
+  safe_system "mv #{name} #{ENV['CC_BUILD_ARTIFACTS']}/#{name}" if ENV['CC_BUILD_ARTIFACTS']
 end
 
 make "clean"
 make "build"
 make "doc"
-safe_system "mv 'dist/doc/html/haskell-quant/haskell-quant' #{ENV['CC_BUILD_ARTIFACTS']}/doc"
+safe_system "mv 'dist/doc/html/haskell-quant/haskell-quant' #{ENV['CC_BUILD_ARTIFACTS']}/doc" if ENV['CC_BUILD_ARTIFACTS']
 
 make "test"
 safe_system "ln -s hpc_index.html test-coverage/index.html"
-safe_system "mv 'test-coverage' #{ENV['CC_BUILD_ARTIFACTS']}/test-coverage"
+safe_system "mv 'test-coverage' #{ENV['CC_BUILD_ARTIFACTS']}/test-coverage" if ENV['CC_BUILD_ARTIFACTS']
 
 
 make "runmain"
 safe_system "ln -s hpc_index.html main-coverage/index.html"
-safe_system "mv 'main-coverage' #{ENV['CC_BUILD_ARTIFACTS']}/main-coverage"
+safe_system "mv 'main-coverage' #{ENV['CC_BUILD_ARTIFACTS']}/main-coverage" if ENV['CC_BUILD_ARTIFACTS']
