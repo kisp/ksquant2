c6242347ee9e48608411ec4027ef7c5f9a06ed71
Kilian Sprotte <kilian.sprotte@gmail.com>
2010-08-09 09:08:32 +0200

    version 0.1.3

--- ksquant2.asd
+++ ksquant2.asd
@@ -1,11 +1,11 @@
 ;;; -*- Mode:Lisp; Syntax:ANSI-Common-Lisp  -*-
 
 (asdf:defsystem ksquant2
-  :version "0.1.2"
+  :version "0.1.3"
   :description " Score quantization for PWGL - successor of ksquant"
   :maintainer "Kilian Sprotte <kilian.sprotte@gmail.com>"
   :author "Kilian Sprotte <kilian.sprotte@gmail.com>"
-  :depends-on (ompw)
+  :depends-on (ompw ksquant) ;temp dep on ksquant
   :serial t
   :components ((:module "lisp"
 			:components ((:file "package")
--- lisp/boxes.lisp
+++ lisp/boxes.lisp
@@ -5,6 +5,44 @@
 (define-menu ksquant2)
 (in-menu ksquant2)
 
+(defun format-time-stamp (&optional (utime (get-universal-time)))
+  (multiple-value-bind (sc mn hr d m y day) (decode-universal-time utime)
+    (declare (ignore day))
+    (format nil "~2,'0D~2,'0D~2,'0D_~2,'0D~2,'0D~2,'0D" (mod y 100) m d hr mn sc)))
+
+(defun report-bug (code sf-path enp-path)
+  (macrolet ((form (form)
+	       `(progn
+		  (princ ',form out)
+		  (write-string " => " out)
+		  (princ ,form out)
+		  (terpri out))))
+    (with-open-file (out "/tmp/report" :direction :output
+			 :if-exists :supersede)
+      (format out "time: ~A~%" (format-time-stamp))
+      (format out "code: ~A~%" code)
+      (format out "lw: ~A ~A~%" (lisp-implementation-type) (lisp-implementation-version))
+      (format out "ksquant version: ~A~%" (asdf:component-version (asdf:find-system :ksquant)))
+      (format out "ksquant2 version: ~A~%" (asdf:component-version (asdf:find-system :ksquant2)))
+      (format out "pwgl version: ~A~%" (sys::pwgl-version-text))
+      (form (probe-file "/usr/lib/libSystem.B.dylib"))
+      (form (probe-file "/opt/local/lib/libgmp.10.dylib"))
+      (form (probe-file "/usr/lib/libgcc_s.1.dylib"))
+      (form (probe-file "/opt/"))
+      (form (with-output-to-string (*standard-output*) (sys::call-system-showing-output "uname -a")))))
+  (unless (probe-file enp-path) (with-open-file (out enp-path :direction :output) (write-line "did not exist" out)))
+  (unless (probe-file sf-path) (with-open-file (out sf-path :direction :output) (write-line "did not exist" out)))
+  (sys::call-system (format nil "tar cfz /tmp/report.tgz /tmp/report ~A ~A" sf-path enp-path))
+  (capi:display-message "Report has been generated. Please choose a place where to save it.")
+  (let ((path (capi:prompt-for-file "Where to save report?"
+				    :operation :save
+				    :filters nil
+				    :pathname (merge-pathnames (make-pathname :name (format nil "ksquant2-report-~A" (format-time-stamp))
+									      :type "tgz"
+									      :directory '(:relative "Desktop"))
+							       (user-homedir-pathname)))))
+    (sys::call-system (format nil "mv /tmp/report.tgz '~A'" path))))
+
 (define-box simple2score ((simple (0 1 2 3))
 			  &key
 			  (time-signatures (4 4))
@@ -13,8 +51,15 @@
 			  (max-div 8)
 			  (forbidden-divs (7)))
   :non-generic t
-  (let ((*default-pathname-defaults* (asdf:component-pathname (asdf:find-system :ksquant2))))
-    (with-open-file (out "/tmp/ksquant2" :direction :output :if-exists :supersede)
+  (let* ((*default-pathname-defaults* (asdf:component-pathname (asdf:find-system :ksquant2)))
+	 (sf-path (format nil "/tmp/ksquant2-~A" (sys::getpid)))
+	 (enp-path (format nil "/tmp/ksquant2-out-~A" (sys::getpid)))
+	 (kernel-path (namestring (merge-pathnames "kernel")))
+	 (simple (ksquant::simple-change-type* :score simple)))
+    (unless (probe-file kernel-path)
+      (error "Cannot find kernel! Have you installed the binary version? ~
+	      Or compiled the kernel yourself?"))
+    (with-open-file (out sf-path :direction :output :if-exists :supersede)
       (with-standard-io-syntax
 	(write
 	 `(:simple
@@ -25,9 +70,28 @@
 	   :max-div ,max-div
 	   :forbidden-divs ,forbidden-divs)
 	 :stream out)))
-    (assert (zerop (sys:call-system (format nil "'~A' </tmp/ksquant2 >/tmp/ksquant2.out"
-					    (namestring (merge-pathnames "bin/ksquant2-kernel"))))))
-    (with-open-file (in "/tmp/ksquant2.out")
+    (let ((code (sys:call-system
+		 (format nil "'~A' <~A >~A" kernel-path sf-path enp-path))))
+      (unless (and nil (zerop code))
+	(if (capi:prompt-for-confirmation
+	     (format nil "The ksquant2 kernel has exited with an error. ~
+			  Please report this bug (by answering No). ~
+			~2%Do you want to try with ksquant version 1 instead?")
+	     :default-button :ok)
+	    (return-from simple2score
+	      (ksquant:simple2score simple
+				    :time-signatures time-signatures
+				    :metronomes metronomes
+				    :scale scale
+				    :max-div max-div
+				    :forbidden-divs forbidden-divs))
+	    (progn
+	      (when (capi:prompt-for-confirmation
+		     "Do you want to generate a bug report?"
+		     :default-button :ok)
+		(report-bug code sf-path enp-path))
+	      (abort)))))
+    (with-open-file (in enp-path)
       (ccl::make-score (read in)))))
 
 (install-menu ksquant2)
