16e936d45bf4aeccc52c52d537bba550f3cd1580
Kilian Sprotte <kilian.sprotte@gmail.com>
2010-10-23 16:31:47 +0200

    added required LANGUAGE pragmas

--- SimpleFormat2.hs
+++ SimpleFormat2.hs
@@ -14,6 +14,9 @@
 
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+{-# LANGUAGE TypeSynonymInstances, MultiParamTypeClasses, FlexibleContexts #-}
+
 module SimpleFormat2 (toSimpleFormat2
                      ,Score
                      ,Part
