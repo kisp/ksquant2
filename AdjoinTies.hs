206a1bd866503b89c362d12076e819bb6835654a
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-12-17 07:17:52 +0000

    small cosmetic change

--- AdjoinTies.hs
+++ AdjoinTies.hs
@@ -15,7 +15,8 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-module AdjoinTies(adjoinTies) where
+module AdjoinTies(adjoinTies)
+where
 import Measure
 import Data.Ratio
 import Lisp
