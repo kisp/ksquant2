24e07c41894885b133846c7c030a474c4ce96c56
Kilian Sprotte <kilian.sprotte@gmail.com>
2018-03-21 16:27:03 +0100

    avoid warning

--- MeasureToDurs.hs
+++ MeasureToDurs.hs
@@ -15,7 +15,7 @@ vToDurs v = L.toSexp $ concatMap mToDurs v
 
 mToDurs :: M.M -> [L.LispVal]
 mToDurs m = map L.toSexp $ concat (zipWith3 t (zip factors durs) ties notes)
-  where factors = map (M.leafLRAs 1 (-1)) $ M.mleaves m
+  where factors = map (M.leafLRAs (1 :: Integer) (-1)) $ M.mleaves m
         durs = map (4*) (M.leafEffectiveDurs $ M.mroot m)
         ties = map M.tied $ M.mleaves m
         notes = map M.enotes $ M.mleaves m
@@ -23,6 +23,6 @@ mToDurs m = map L.toSexp $ concat (zipWith3 t (zip factors durs) ties notes)
                           | otherwise = [L.toSexp [L.toSexp b, L.readLisp' ":notes", ns]]
         t (1, b) True ns | ns == L.n60 = [L.toSexp b, tie]
                          | otherwise = [L.toSexp [L.toSexp b, L.readLisp' ":notes", ns], tie]
-        t ((-1), b) False _ = [L.toSexp $ (-1) * b]
-        t ((-1), _) True _ = error "a rest should not have a tie"
+        t (-1, b) False _ = [L.toSexp $ (-1) * b]
+        t (-1, _) True _ = error "a rest should not have a tie"
         t (_, _) _ _ = error "not -1 or 1 should not happen for fst of first arg"
