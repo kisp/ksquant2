1b0aeb7a1900ffce0cabcf8078859e1ecf7eaf4b
Kilian Sprotte <kilian.sprotte@gmail.com>
2018-03-21 23:37:26 +0100

    fix Lisp.hs for GHC 7.0.4

--- Lisp.hs
+++ Lisp.hs
@@ -15,7 +15,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 
 module Lisp (Sexp
